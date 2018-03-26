e76b28b51891936809d229eddc72ba9246196e3b
vpsa23 <vpsa23@hotmail.com>
2018-03-26 12:17:41 -0300

    Haciendo cambios version-mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
   // init controller
   var controller = new ScrollMagic.Controller();
 
+//Imagen de aboutMe
   new ScrollMagic.Scene({
       triggerElement: '#me'
     })
@@ -15,7 +16,46 @@ $(document).ready(() => {
     })
     .setClassToggle('#list1', 'show')
     .addTo(controller);
+
+    new ScrollMagic.Scene({
+      triggerElement: '#list2'
+    })
+    .setClassToggle('#list2', 'show')
+    .addTo(controller);
+
+    new ScrollMagic.Scene({
+      triggerElement: '#cardSection'
+    })
+    .setClassToggle('#cardSection', 'fade-in')
+    .addTo(controller);
+
+    new ScrollMagic.Scene({
+      triggerElement: '#cardSection1'
+    })
+    .setClassToggle('#cardSection1', 'fade-in')
+    .addTo(controller);
+
+    new ScrollMagic.Scene({
+      triggerElement: '#listContact'
+    })
+    .setClassToggle('#listContact', 'fade-in')
+    .addTo(controller);
+
+    new ScrollMagic.Scene({
+      triggerElement: '#listImg'
+    })
+    .setClassToggle('#listImg', 'fade-in')
+    .addTo(controller);
+
+    new ScrollMagic.Scene({
+      triggerElement: '#listImg2'
+    })
+    .setClassToggle('#listImg2', 'fade-in')
+    .addTo(controller);
+
+
 });
+
 
 
 // Evento del menú
